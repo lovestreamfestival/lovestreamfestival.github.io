2f0c1205cb59116abd0dce5c531bb4df8ea7c24e
Tarik Kavaz <tarikkavaz@gmail.com>
2021-11-03 16:14:02 +0300

    Action test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-screen mx-auto text-center">
-    <h1 class="text-4xl">Under Construction</h1>
+    <h1 class="text-4xl">Under Construction!</h1>
   </div>
 </template>
 
